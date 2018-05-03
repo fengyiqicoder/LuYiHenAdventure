7767d61025c60f455f1698b4d2a9baf74a96b961
FinchFeng <329650437@qq.com>
2018-05-03 13:32:20 +0800

    游戏完成！等待调试

--- AnimatedBearSwift/Contoller/GameViewController.swift
+++ AnimatedBearSwift/Contoller/GameViewController.swift
@@ -1,6 +1,7 @@
 
 import UIKit
 import SpriteKit
+import AVKit
 
 var scene:GameScene!
 
@@ -12,8 +13,56 @@ class GameViewController: UIViewController,ScenseDelegate {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    if isFirstLaunch(){
+      playVideoAndStartGame()
+    }else{
+      restartGame()
+    }
+  }
+  func isFirstLaunch() -> Bool {
+    if !(UserDefaults.standard.bool(forKey: "HasLaunchedOnce")) {
+      // This is the first launch ever
+      print("my first time launch")
+      //set date to today and date value
+      UserDefaults.standard.set(true, forKey: "HasLaunchedOnce")//set the launch value
+      return true
+    }
+    return false
+  }
+  //游戏开始
+  var playerLayer:AVPlayerLayer!
+  var blackBackGroundView:UIView!
+  func playVideoAndStartGame(){
+    //定义一个视频文件路径
+    let filePath = Bundle.main.path(forResource: "gameStartVideo", ofType: "mp4")
+    let videoURL = URL(fileURLWithPath: filePath!)
+    //定义一个playerItem，并监听相关的通知
+    let playerItem = AVPlayerItem(url: videoURL)
+    NotificationCenter.default.addObserver(self,
+                                           selector: #selector(playerDidFinishPlaying),
+                                           name: NSNotification.Name.AVPlayerItemDidPlayToEndTime,
+                                           object: playerItem)
+    //定义一个视频播放器，通过playerItem径初始化
+    let player = AVPlayer(playerItem: playerItem)
+    //设置大小和位置（全屏）
+    playerLayer = AVPlayerLayer(player: player)
+    playerLayer.frame = self.view.bounds
+    //添加背景
+    blackBackGroundView = UIView(frame: self.view.bounds)
+    blackBackGroundView.backgroundColor = UIColor.black
+    self.view.addSubview(blackBackGroundView)
+    //添加到界面上
+    self.view.layer.addSublayer(playerLayer)
+    //开始播放
+    player.play()
+  }
+  
+  @objc func playerDidFinishPlaying(){
+    blackBackGroundView.removeFromSuperview()
+    playerLayer.removeFromSuperlayer()
     restartGame()
   }
+  
   
   @IBOutlet weak var gameRestartButton: UIButton!
   @IBOutlet weak var gameWinningImageView: UIImageView!
@@ -36,8 +85,8 @@ class GameViewController: UIViewController,ScenseDelegate {
       scene.viewControllerDelegate = self
       scene.scaleMode = .resizeFill
       view.ignoresSiblingOrder = true
+      //完成之后进行更改
       view.showsFPS = true
-//      view.showsNodeCount = true
       view.presentScene(scene)
       bloodBar.blood = 100
       //获取关卡名称
